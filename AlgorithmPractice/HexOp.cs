b909dd28aeeab5c0e1956c0603103aa425566c64
jtaylor <james.jm.taylor@gmail.com>
2019-12-21 11:43:54 -0800

    Started work on hex & bytes.  Added new unit test class to prove out concepts

---
+++ AlgorithmPractice/HexOp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPractice
+{
+    //NOTE: Hex value is not a datatype in C or C# (although bytes are).
+    //The computer converts passed hex values to binary representations of ints
+    public class HexOp
+    {
+        public static int GetIntForHex(int a)
+        {
+            return a;
+        }
+
+        public static String GetHexForInt(int a)
+        {
+            var bytes = BitConverter.GetBytes(a);
+            return BitConverter.ToString(bytes);
+        }
+
+        public static Boolean IsLittleEndian()
+        {
+            return BitConverter.IsLittleEndian;
+        }
+
+    }
+}
---
+++ AlgorithmUnitTests/HexUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmPractice;
+
+namespace AlgorithmUnitTests
+{
+    [TestClass]
+    public class HexToIntUnitTests
+    {
+        [TestMethod]
+        public void GetIntForZeroHex()
+        {
+            Assert.AreEqual(0, HexOp.GetIntForHex(0x00000000)); 
+        }
+
+        [TestMethod]
+        public void GetIntForTenHex()
+        {
+            Assert.AreEqual(10, HexOp.GetIntForHex(0x0000000A));
+        }
+
+        [TestMethod]
+        public void GetHexForTen()
+        {
+            Assert.AreEqual("0A-00-00-00", HexOp.GetHexForInt(10));
+        }
+        [TestMethod]
+        public void GetHexForTwoFiftyFive()
+        {
+            Assert.AreEqual("FF-00-00-00", HexOp.GetHexForInt(255));
+        }
+        [TestMethod]
+        public void GetHexForNegativeValue()
+        {
+            Assert.AreEqual("D1-FF-FF-FF", HexOp.GetHexForInt(-47));
+        }
+
+        //NOTE: C#'s BitConverter library is little endian, even on x86 
+        //(as shown by the Unit Test below) 
+        //This is why the hex values above seem backwards.
+        [TestMethod]
+        public void IsLittleEndian()
+        {
+            Assert.AreEqual(true, HexOp.IsLittleEndian());
+        }
+    }
+}
